f62512d4beebc2c3ac601d0e5a2093bb20251198
Gustavo Lemos <galaxywander0@gmail.com>
2023-08-01 12:04:43 -0300

    arrumei a potencia

--- Aula 3/Aquario.java
+++ Aula 3/Aquario.java
@@ -6,6 +6,8 @@ public class Aquario {
          double comprimento = 0.0;
          double altura = 0.0;
          double largura = 0.0;
+         double temperaturaDesejada = 0.0;
+         double temperaturaAmbiente = 0.0;
 
          System.out.println("Digite o comprimento do seu aquário: ");
          comprimento = sn.nextDouble();
@@ -13,21 +15,26 @@ public class Aquario {
          altura = sn.nextDouble();
          System.out.println("Digite a largura do seu aquário: ");
          largura = sn.nextDouble();
+         System.out.println("Digite a temperatura desejada para o seu aquário: ");
+         temperaturaDesejada = sn.nextDouble();
+         System.out.println("Digite a temperatura ambientoe: ");
+         temperaturaAmbiente = sn.nextDouble();
          double volume = calcularVolume(comprimento, altura, largura);
 
-         System.out.printf("O volume do seu aquário é de %f litros.", volume);
-         System.out.printf("A quantidade de filtragem por hora é de %f litros.", calcularFiltragem(volume));
+         System.out.printf("O volume do seu aquário é de %d litros.\n", Math.round(volume));
+         System.out.printf("A quantidade de filtragem por hora é de %d litros ou de %d litros.\n", Math.round(calcularFiltragem(volume)[0]), Math.round(calcularFiltragem(volume)[1]) );
+         System.out.printf("A potencia do seu aquário deve ser de %d.", Math.round(calcularPotencia(volume, temperaturaDesejada, temperaturaAmbiente)));
          sn.close();
 
     }
 
     public static double calcularPotencia (double volume, double temperaturaDesejada, double temperaturaAmbiente){
-        
-        return  0.0;
+        double potencia = volume * 0.5 * (temperaturaDesejada - temperaturaAmbiente);
+        return  potencia;
     }
 
-    public static double calcularFiltragem (double volume){
-        double filtragem = volume * 2.5;
+    public static double[] calcularFiltragem (double volume){
+        double[] filtragem = {volume * 2, volume * 3};
         return filtragem;
     }
 
